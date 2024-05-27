2898bafb9029eeea2acc53100193700748742d4d
jessyjyr <jessyyr02@gmail.com>
2024-05-27 12:36:57 +0200

    estado inicial del proyecto

---
+++ src/main/java/com/example/TiposDeDatosYOperadores/App.java
@@ -0,0 +1,13 @@
+package com.example.TiposDeDatosYOperadores;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
